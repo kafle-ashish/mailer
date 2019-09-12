39517d803e339c6c6b93af672a2c093d0b1a16dc
codedump00 <blazinasis@gmail.com>
2019-09-12 12:03:22 +0545

    finished mailer

--- apps/mailer/mailer.controllers.js
+++ apps/mailer/mailer.controllers.js
@@ -1,24 +1,26 @@
-const nodemailer = require("nodemailer")
-const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.USER_EMAIL,
-        pass: process.env.USER_PASSWORD
-    }
-})
+const transporter = require("../../config/transporter")
 
 const send = async (req, res) => {
     try {
-        const data = await transporter.sendMail({
-            from: "verifier@lightweb.com",
-            to: req.body.to,
-            subject: req.body.subject,
-            text: req.body.text
-        })
-        return res.status(200).json({
-            message: "Mail sent!",
-            result: data
-        })
+        transporter.sendMail(
+            {
+                from: "user.verifier.bot@gmail.com",
+                to: req.body.to,
+                subject: req.body.subject,
+                text: req.body.text
+            },
+            (err, data) => {
+                if (err)
+                    return res.status(400).json({
+                        error: "Error ocurred!",
+                        err: err
+                    })
+                return res.status(200).json({
+                    message: "Mail sent!",
+                    result: data
+                })
+            }
+        )
     } catch (e) {
         return res.status(400).json({
             error: "Error ocurred!",
